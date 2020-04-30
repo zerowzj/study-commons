3910b58f0591413df799c03ed1765301520da7de
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-04-30 15:37:06 +0800

    更新代码

--- study-exception/src/main/java/study/exception/business/AlreadyExistException.java
+++ study-exception/src/main/java/study/exception/business/AlreadyExistException.java
@@ -9,11 +9,6 @@ public class AlreadyExistException extends BusinessException {
 
     private static final String ERROR_CODE = "2002";
 
-    /**
-     * 构造函数
-     *
-     * @param objName
-     */
     public AlreadyExistException(String objName) {
         super(ERROR_CODE, String.format("[{}]已存在", objName));
     }
--- study-exception/src/main/java/study/exception/business/ErrorException.java
+++ study-exception/src/main/java/study/exception/business/ErrorException.java
@@ -9,11 +9,6 @@ public class ErrorException extends BusinessException {
 
     private static final String ERROR_CODE = "2003";
 
-    /**
-     * 构造函数
-     *
-     * @param objName
-     */
     public ErrorException(String objName) {
         super(ERROR_CODE, String.format("[{}]错误", objName));
     }
